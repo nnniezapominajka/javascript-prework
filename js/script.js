6cd6ffbaae7f214d7e13a4495adaa61ed1a37dfb
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-02-19 22:57:58 +0100

    Change

--- js/script.js
+++ js/script.js
@@ -30,7 +30,25 @@ if(playerInput == '1'){
 
 printMessage('Twój ruch to: ' + playerMove);
 
-
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');  
+}else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');  
+}else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');  
+}else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!'); 
+}else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');     
+}else if (computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+    printMessage('Brak wyniku!');  
+}else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+    printMessage('Brak wyniku!'); 
+}else if (computerMove == 'papier' && playerMove == 'nieznany ruch'){
+    printMessage('Brak wyniku!'); 
+}     
 
 
 

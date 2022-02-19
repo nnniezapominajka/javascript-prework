ff9e156a0b0c554699c162f20739b6ac115bccd2
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-02-19 21:54:59 +0100

    Add change

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'nieznany ruch';
+let computerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
-else if(playerInput=='2'){
-  playerMove = 'papier';  
+else if(randomNumber == 2){
+   computerMove = 'papier'; 
 }
-else if (playerInput=='3') {
-  playerMove = 'nożyce';  
-}
+else if (randomNumber == 3){
+   computerMove = 'nożyce'; 
+} 
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
+
 
 
 

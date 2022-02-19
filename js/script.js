fd2056ee9aad8494c893443ca4af37df62298aef
Małgorzata Kopydłowska <niezapominajka@kodilla.com>
2022-02-19 21:09:10 +0100

    Add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-let playerMove = `papier`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput=='2'){
+  playerMove = 'papier';  
+}
+else if (playerInput=='3') {
+  playerMove = 'nożyce';  
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+
+
+
+
+
+
+
+
+
+
